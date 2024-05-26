e6404aadcfe22c83a46c9632853560ff6ce010c5
victor javier Vivas <victorjaviervivas@gmail.com>
2024-05-26 13:42:45 -0400

    Se quita en UserDtoModify que el password sea requerida y que se pueda modificar

--- music-house-back/src/main/java/com/musichouse/api/music/dto/dto_modify/UserDtoModify.java
+++ music-house-back/src/main/java/com/musichouse/api/music/dto/dto_modify/UserDtoModify.java
@@ -28,7 +28,6 @@ public class UserDtoModify {
     @Email(message = "El email debe ser válido")
     private String email;
 
-    @NotBlank(message = "La contraseña es obligatoria")
     @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
     private String password;
 
